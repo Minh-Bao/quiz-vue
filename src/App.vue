db6217399ea85771f39797e532355cbbcc3f1ab8
minh-bao <mbnguyen@hotmail.fr>
2021-05-10 19:23:13 +0200

    defne quiz main page style

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,20 @@
 <template>
-    <div id="app">
-        <h1>Un quiz ?</h1>
+    <div id="app" class="container">
+        <h1 class="mb-4">Un quiz ?</h1>
+        <b-card
+            header="Quel révolutionnaire et grand orateur a déclaré en 1792 : “De l’audace, encore de l’audace, toujours de l’audace.”"
+            header-tag="header">
+            <b-list-group>
+                <b-list-group-item button>Desmoulin</b-list-group-item>
+                <b-list-group-item button>Danton</b-list-group-item>
+                <b-list-group-item button>Robespierre</b-list-group-item>
+                <b-list-group-item button>Saint Just</b-list-group-item>
+            </b-list-group>
+        </b-card>
     </div>
 </template>
 
 <script>
-
 export default {
     name: "App",
 };
