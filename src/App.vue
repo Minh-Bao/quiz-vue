060474f449381c14f860e638d3528e2ed952e80b
minh-bao <mbnguyen@hotmail.fr>
2021-05-11 14:26:23 +0200

    edit app.vue file according to vueX config

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,24 @@
 <template>
     <div id="app" class="container">
-        <h1 class="mb-4">Un quiz ?</h1>
-        <b-alert v-if="fin" show>
-            Votre score est : {{ score }} / {{ questions.length }}</b-alert
+        <h1 class="mb-4">Un petit quiz</h1>
+        <b-alert v-if="fin" show
+            >Votre score est : {{ score }} / {{ questions.length }}</b-alert
         >
         <b-card :header="questions[index].question" header-tag="header">
             <b-list-group>
                 <b-list-group-item
                     button
                     v-for="(item, index) in questions[index].answers"
-                    :key="item"
+                    :key="item.id"
                     @click="action(index)"
+                    :variant="variants[index]"
                 >
                     {{ item }}
                 </b-list-group-item>
             </b-list-group>
             <b-button v-if="fin" @click="recommencer" class="mt-4"
-                >Recommencer !
-            </b-button>
+                >Recommencer !</b-button
+            >
             <b-button v-if="voirReponse && !fin" @click="continuer" class="mt-4"
                 >Continuer !</b-button
             >
@@ -27,38 +28,14 @@
 
 <script>
 export default {
-    name: "App",
+    name: "app",
     data: function () {
         return {
-            variants: [...Array(4)],
-            voirReponse: false,
             fin: false,
             index: 0,
             score: 0,
-            questions: [
-                {
-                    question:
-                        "Quel révolutionnaire et grand orateur a déclaré en 1792 : “De l’audace, encore de l’audace, toujours de l’audace.”",
-                    answers: [
-                        "Desmoulin",
-                        "Danton",
-                        "Robespierre",
-                        "Saint Just",
-                    ],
-                    ok: 1,
-                },
-                {
-                    question:
-                        "Dans quel pays peut-on trouver le mont Elbrouz ?",
-                    answers: ["Russie", "Azerbaïdjan", "Géorgie", "Iran"],
-                    ok: 0,
-                },
-                {
-                    question: "Qui a dit “Ich bin ein Berliner” ?",
-                    answers: ["Bismarck", "Reagan", "De Gaulle", "Kennedy"],
-                    ok: 3,
-                },
-            ],
+            variants: [...Array(4)],
+            voirReponse: false,
         };
     },
     methods: {
@@ -85,6 +62,14 @@ export default {
             this.index++;
         },
     },
+    computed: {
+        questions() {
+            return this.$store.state.questions;
+        },
+    },
+    created() {
+        this.$store.dispatch('getData');
+    }
 };
 </script>
 
