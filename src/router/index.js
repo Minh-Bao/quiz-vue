64aa7eeb3e2d10b99339e640f357503a5f128b4b
minh-bao <mbnguyen@hotmail.fr>
2021-05-11 22:11:52 +0200

    add route for action

---
+++ src/router/index.js
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+import Action from '../views/Action.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/action/:id',
+    name: 'action',
+    component: Action
+  }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
