fc4677f4cb402accc057fb2d997820f88d668367
minh-bao <mbnguyen@hotmail.fr>
2021-05-11 22:13:25 +0200

    update app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,77 +1,8 @@
 <template>
-    <div id="app" class="container">
-        <h1 class="mb-4">Un petit quiz</h1>
-        <b-alert v-if="fin" show
-            >Votre score est : {{ score }} / {{ questions.length }}</b-alert
-        >
-        <b-card :header="questions[index].question" header-tag="header">
-            <b-list-group>
-                <b-list-group-item
-                    button
-                    v-for="(item, index) in questions[index].answers"
-                    :key="item.id"
-                    @click="action(index)"
-                    :variant="variants[index]"
-                >
-                    {{ item }}
-                </b-list-group-item>
-            </b-list-group>
-            <b-button v-if="fin" @click="recommencer" class="mt-4"
-                >Recommencer !</b-button
-            >
-            <b-button v-if="voirReponse && !fin" @click="continuer" class="mt-4"
-                >Continuer !</b-button
-            >
-        </b-card>
+    <div id="app">
+        <router-view />
     </div>
 </template>
-
-<script>
-export default {
-    name: "app",
-    data: function () {
-        return {
-            fin: false,
-            index: 0,
-            score: 0,
-            variants: [...Array(4)],
-            voirReponse: false,
-        };
-    },
-    methods: {
-        action: function (index) {
-            // Test bonne réponse
-            if (index == this.questions[this.index].ok) {
-                this.score++;
-            } else {
-                this.variants[index] = "danger";
-            }
-            this.voirReponse = true;
-            this.variants[this.questions[this.index].ok] = "success";
-            if (this.index == this.questions.length - 1) {
-                this.fin = true;
-            }
-        },
-        recommencer: function () {
-            this.voirReponse = this.fin = this.index = this.score = 0;
-            this.variants = [...Array(4)];
-        },
-        continuer: function () {
-            this.voirReponse = false;
-            this.variants = [...Array(4)];
-            this.index++;
-        },
-    },
-    computed: {
-        questions() {
-            return this.$store.state.questions;
-        },
-    },
-    created() {
-        this.$store.dispatch('getData');
-    }
-};
-</script>
 
 <style>
 #app {
@@ -80,6 +11,5 @@ export default {
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
     color: #2c3e50;
-    margin-top: 60px;
 }
 </style>
