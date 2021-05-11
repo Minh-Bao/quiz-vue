6faf73b9d418a53b97f97271ea585fa3c085158d
minh-bao <mbnguyen@hotmail.fr>
2021-05-11 22:12:43 +0200

    action vue

---
+++ src/views/Action.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <quiz />
+    </div>
+</template>
+
+<script>
+import Quiz from "@/components/Quiz.vue";
+
+export default {
+    name: "action",
+    components: {
+        Quiz,
+    },
+};
+</script>
