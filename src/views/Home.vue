09b719ef038ac89f20a0834b780cc43017bfd2de
minh-bao <mbnguyen@hotmail.fr>
2021-05-11 22:13:11 +0200

    home view thaht display quiz list

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="home">
+        <img alt="logo" class="mb-4" src="../assets/quiz.jpg" />
+        <quizs />
+    </div>
+</template>
+
+<script>
+import Quizs from "@/components/Quizs.vue";
+
+export default {
+    name: "home",
+    components: {
+        Quizs,
+    },
+};
+</script>
